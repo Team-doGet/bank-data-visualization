1cc0a1e749cd66e0eb0e0466767a67d55dd05e3e
Simin-Kim <show3391@gmail.com>
2023-05-04 10:40:58 +0900

    feat: add FinancialService #13

---
+++ server/src/site/doget/service/FinancialService.java
@@ -0,0 +1,52 @@
+package site.doget.service;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import site.doget.common.CodeMapper;
+import site.doget.dto.FinancialListResDto;
+import site.doget.dto.FinancialReqDto;
+import site.doget.dto.FinancialResDto;
+import site.doget.dto.FnnrRawDto;
+import site.doget.mybatis.mapper.FinancialMapper;
+import site.doget.mybatis.SqlSessionFactoryProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FinancialService {
+    private static final FinancialService instance = new FinancialService();
+    public static FinancialService getInstance() {return instance;}
+    private FinancialService() {
+    }
+
+    private final SqlSessionFactory sqlSessionFactory = SqlSessionFactoryProvider.getInstance();
+
+    public FinancialListResDto findByTermAndBankCode(FinancialReqDto financialReqDto) {
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            FinancialMapper financialMapper = sqlSession.getMapper(FinancialMapper.class);
+            List<FnnrRawDto> fnnrRawDtoList = financialMapper.findByTermAndBankCode(financialReqDto);
+
+            // FinancialListResDto
+            List<String> date = new ArrayList<>();
+            List<FinancialResDto> datasets = new ArrayList<>();
+
+            // FinancialResDto
+            String[] codes = CodeMapper.financialMap.keySet().toArray(new String[0]);
+            Arrays.sort(codes);
+
+            for (String code : codes) {
+                datasets.add(new FinancialResDto(code, CodeMapper.financialMap.get(code), new ArrayList<>()));
+            }
+
+            for (FnnrRawDto fnnrRawDto : fnnrRawDtoList) {
+                date.add(fnnrRawDto.getBaseYm());
+                for (int i = 0; i < codes.length; i++) {
+                    datasets.get(i).getData().add(fnnrRawDto.getValueByCode(codes[i]));
+                }
+            }
+
+            return new FinancialListResDto(date, datasets);
+        }
+    }
+}
