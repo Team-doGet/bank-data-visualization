3340ee119f238e893c5005e7b76f8c505dc3be4d
s-yeong <syxxng25@gmail.com>
2023-05-04 20:10:04 +0900

    fix: mapper query 수정

---
+++ server/src/site/doget/dto/CustomerCountByAmountRawDto.java
@@ -0,0 +1,13 @@
+package site.doget.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustTypeRawDto {
+
+    private String newDt;
+    private String custDscdNm;
+    private String count;
+}
