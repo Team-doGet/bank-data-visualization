386c063b33932569cfb6b44a28953926e2de61cb
Simin-Kim <show3391@gmail.com>
2023-05-05 22:11:38 +0900

    feat: add LoanGuaranteeDto files #39

---
+++ server/src/site/doget/dto/LoanGuaranteeResDto.java
@@ -0,0 +1,15 @@
+package site.doget.dto;
+
+import lombok.Getter;
+import java.util.ArrayList;
+
+@Getter
+public class LoanGuaranteeResDto {
+    private String label;
+    private ArrayList<Integer> data;
+
+    public LoanGuaranteeResDto(String label, ArrayList<Integer> data) {
+        this.label = label;
+        this.data = data;
+    }
+}
