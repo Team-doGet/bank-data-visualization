076121716c62f0d9a161f0678af05acf60a0429b
Simin-Kim <show3391@gmail.com>
2023-05-05 22:12:21 +0900

    feat: add DepositTypeDto files #39

---
+++ server/src/site/doget/dto/DepositTypeResDto.java
@@ -0,0 +1,16 @@
+package site.doget.dto;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class DepositTypeResDto {
+    private String label;
+    private ArrayList<Integer> data;
+
+    public DepositTypeResDto(String label, ArrayList<Integer> data) {
+        this.label = label;
+        this.data = data;
+    }
+}
