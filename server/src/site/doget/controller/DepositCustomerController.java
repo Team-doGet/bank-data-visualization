69540f34383712a8cbbf5da6548dd06c8ea4eb63
s-yeong <syxxng25@gmail.com>
2023-05-05 19:53:23 +0900

    feat: 지역별 예금 고객 수 조회 #46

--- server/src/site/doget/controller/DepositCustomerController.java
+++ server/src/site/doget/controller/DepositCustomerController.java
@@ -3,6 +3,7 @@ package site.doget.controller;
 import site.doget.common.ApiResponse;
 import site.doget.common.ValidationResult;
 import site.doget.dto.BankReqDto;
+import site.doget.dto.CustomerCountByRegionDto;
 import site.doget.dto.CustomerCountListByAmountResDto;
 import site.doget.dto.CustomerCountListByTypeResDto;
 import site.doget.service.DepositCustomerService;
@@ -19,17 +20,22 @@ public class DepositCustomerController implements Controller {
 
         if(requestURI.endsWith("/type")) {
             return findDepositByCustomerType(paramMap);
-        } else if (requestURI.endsWith("/amount")) {
+        }
+        else if (requestURI.endsWith("/amount")) {
             return findDepositByAmount(paramMap);
-        } else {
+        }
+        else if (requestURI.endsWith("/region")) {
+            return findDepositByRegion(paramMap);
+        }
+        else {
             return new ApiResponse<>(HttpServletResponse.SC_NOT_FOUND, "요청한 URI가 존재하지 않습니다.");
         }
     }
 
+    // 고객 종류별 예금 고객 수 조회
     public ApiResponse<?> findDepositByCustomerType(Map<String, String> paramMap) {
 
-        BankReqDto bankReqDto = new BankReqDto(paramMap.get("bankCode"), paramMap.get("term"),
-                paramMap.get("stDate"), paramMap.get("endDate"));
+        BankReqDto bankReqDto = createBankReqDto(paramMap);
 
         ValidationResult validationResult = bankReqDto.validate();
 
@@ -42,10 +48,10 @@ public class DepositCustomerController implements Controller {
         return new ApiResponse<>(HttpServletResponse.SC_OK, depositByCustomerType);
     }
 
+    // 금액별 예금 고객 수 조회
     public ApiResponse<?> findDepositByAmount(Map<String, String> paramMap) {
 
-        BankReqDto bankReqDto = new BankReqDto(paramMap.get("bankCode"), paramMap.get("term"),
-                paramMap.get("stDate"), paramMap.get("endDate"));
+        BankReqDto bankReqDto = createBankReqDto(paramMap);
 
         ValidationResult validationResult = bankReqDto.validate();
 
@@ -56,5 +62,26 @@ public class DepositCustomerController implements Controller {
         CustomerCountListByAmountResDto depositByAmount = depositCustomerService.findDepositByAmount(bankReqDto);
 
         return new ApiResponse<>(HttpServletResponse.SC_OK, depositByAmount);
+    }
+
+    // 지역별 예금 고객 수 조회
+    public ApiResponse<?> findDepositByRegion(Map<String, String> paramMap) {
+
+        BankReqDto bankReqDto = createBankReqDto(paramMap);
+
+        ValidationResult validationResult = bankReqDto.validate();
+
+        if (!validationResult.isValid()) {
+            return new ApiResponse<>(HttpServletResponse.SC_BAD_REQUEST, validationResult.getErrorMessage());
+        }
+
+        CustomerCountByRegionDto depositByRegion = depositCustomerService.findDepositByRegion(bankReqDto);
+
+        return new ApiResponse<>(HttpServletResponse.SC_OK, depositByRegion);
+    }
+
+    private BankReqDto createBankReqDto(Map<String, String> paramMap) {
+        return new BankReqDto(paramMap.get("bankCode"), paramMap.get("term"),
+                paramMap.get("stDate"), paramMap.get("endDate"));
     }
 }
