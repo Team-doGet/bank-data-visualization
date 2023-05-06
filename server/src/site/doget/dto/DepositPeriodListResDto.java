a7236cccf44320a63663cdcb09ea542e3ebae055
Simin-Kim <show3391@gmail.com>
2023-05-06 12:40:24 +0900

    feat: implement findPeriod's ApiResponse to Controller #49

--- server/src/site/doget/dto/DepositPeriodListResDto.java
+++ server/src/site/doget/dto/DepositPeriodListResDto.java
@@ -1,2 +1,16 @@
-package site.doget.dto;public class DepositPeriodListResDto {
+package site.doget.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class DepositPeriodListResDto {
+    private List<String> labels;
+    private List<DepositPeriodResDto> datasets;
+
+    public DepositPeriodListResDto(List<String> labels, List<DepositPeriodResDto> datasets) {
+        this.labels = labels;
+        this.datasets = datasets;
+    }
 }
--- server/src/site/doget/dto/DepositPeriodResDto.java
+++ server/src/site/doget/dto/DepositPeriodResDto.java
@@ -1,2 +1,16 @@
-package site.doget.dto;public class DepositPeriodResDto {
+package site.doget.dto;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class DepositPeriodResDto {
+    private String label;
+    private ArrayList<Integer> data;
+
+    public DepositPeriodResDto(String label, ArrayList<Integer> data) {
+        this.label = label;
+        this.data = data;
+    }
 }
